de214f21fd1ce640672a2db5c9046cefce49ae50
Nublao <aj.escudo@gmail.com>
2018-02-06 01:22:30 +0100

    1

--- ajax.js
+++ ajax.js
@@ -1,9 +1,8 @@
 function obtenerJsonGeneral() {
   // Obtener la instancia del objeto XMLHttpRequest
-  if(window.XMLHttpRequest) {
-     peticionHttp = new XMLHttpRequest();
-  }
-  else if(window.ActiveXObject) {
+  if (window.XMLHttpRequest) {
+    peticionHttp = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
     peticionHttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
   // Preparar la funcion de respuesta
@@ -12,9 +11,10 @@ function obtenerJsonGeneral() {
   peticionHttp.open('GET', 'https://omgvamp-hearthstone-v1.p.mashape.com/cards?locale=esES', true);
   peticionHttp.setRequestHeader("X-Mashape-Key", "GeBGm3vFtjmshBynOo5sk35SbMOIp1dwKCljsnNmOwPjk71p9Y");
   peticionHttp.send(null);
+
   function muestraContenido() {
-    if(peticionHttp.readyState == 4) {
-      if(peticionHttp.status == 200) {
+    if (peticionHttp.readyState == 4) {
+      if (peticionHttp.status == 200) {
 
         var respuesta = peticionHttp.responseText;
         objetoJson = eval("(" + respuesta + ")");
@@ -29,33 +29,4 @@ function obtenerJsonGeneral() {
   }
 }
 
-function seleccionarClase(json) {
-  if(window.XMLHttpRequest) {
-    peticionHttp = new XMLHttpRequest();
- }
- else if(window.ActiveXObject) {
-   peticionHttp = new ActiveXObject("Microsoft.XMLHTTP");
- }
- // Preparar la funcion de respuesta
- peticionHttp.onreadystatechange = muestraContenido;
-  var listaClases = document.getElementById('selectListaClases');
-  peticionHttp.open('GET', 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/?locale=esES', true);
-  peticionHttp.setRequestHeader("X-Mashape-Key", "GeBGm3vFtjmshBynOo5sk35SbMOIp1dwKCljsnNmOwPjk71p9Y");
-  peticionHttp.send(null);
-  function muestraClase() {
-    if(peticionHttp.readyState == 4) {
-      if(peticionHttp.status == 200) {
-
-        var respuesta = peticionHttp.responseText;
-        objetoJson = eval("(" + respuesta + ")");
-        var imagen = document.getElementById('imagen');
-        var texto = document.getElementById('texto');
-        imagen.src = objetoJson.Naxxramas[100].img;
-        texto.innerHTML = objetoJson.Naxxramas[100].flavor;
-
-        seleccionarClase(objetoJson);
-      }
-    }
-  }
-}
 window.onload = obtenerJson;
